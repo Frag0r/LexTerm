d7a0c6fcc2e429f8f323a16e0354a098eeee3593
Frag0r <mleschnik@web.de>
2020-05-18 11:55:33 +0200

    Switch to RegEx for String comparison, implemented second extraction method, code refactoring (more functions ^_^)

--- src/extract_terms.py
+++ src/extract_terms.py
@@ -6,6 +6,7 @@ a. Article	Adjective*	Noun;
 b.	Article	Noun	Noun
 """
 from pathlib import Path
+import re
 
 my_file = Path("data/output.txt")
 if my_file.is_file():
@@ -18,28 +19,68 @@ else:
     outputData.close()
     outputData=open("data/output.txt", "a")
 
-inputData=open("data/gnu_de_en__german.vert", "r", encoding="utf8")
+inputData=open("data/gnu_de_en__german.vert", "r", encoding="utf8") #Path to inputfile TO-DO: Include Extraction of other files w/o changing code
 statusCode = 0
+
+regExArtikel = re.compile(r'ART.')
+regExNomen = re.compile(r"N.")
+regExAdjektiv = re.compile(r"ADJA.")
+
+methodInput = input("What kind of extraction do you want ?\na. Article	Adjective*	Noun;\nb.	Article	Noun	Noun\n")
+
+if methodInput == 'a':
+    methodChoice = False
+else:
+    if methodInput == 'b':
+        methodChoice = True
+    else:
+        print("Wrong Input! Restart Script")
+        quit
+    
+
+def checkRegEx(wortart,eingabe):
+    if wortart.match(eingabe):
+        return True
+    else:
+        return False
+
+def termExtraction(method):
+    global statusCode
+    if method:  #Method B
+        if statusCode == 0:
+            if checkRegEx(regExArtikel,entry[1]):
+                outputData.write(term+" ")
+                statusCode = 1
+        if statusCode == 1:
+            if checkRegEx(regExNomen,entry[1]):
+                outputData.write(term+"\n")
+                statusCode = 2
+        if statusCode == 2:
+            if checkRegEx(regExNomen,entry[1]):
+                outputData.write(term+"\n")
+                statusCode = 0
+    else:   #Method A
+        if statusCode == 0:
+            if checkRegEx(regExArtikel,entry[1]):
+                outputData.write(term+" ")
+                statusCode = 1
+        if statusCode == 1:
+            if checkRegEx(regExNomen,entry[1]):
+                outputData.write(term+"\n")
+                statusCode = 0
+            if checkRegEx(regExAdjektiv,entry[1]):
+                outputData.write(term+" ")
+                statusCode = 2
+        if statusCode == 2:
+            if checkRegEx(regExNomen,entry[1]):
+                outputData.write(term+"\n")
+                statusCode = 0
 
 for line in inputData:
     entry = line.split('\t')
     term = entry[0]
+    
     if term.find("<") == -1:
-        if statusCode == 0:
-            if entry[1].find("ART") != -1:
-                outputData.write(term+" ")
-                statusCode = 1
-
-        if statusCode == 1:
-            if entry[1].find("N.") != -1:
-                outputData.write(term+"\n")
-                statusCode = 0
-            if entry[1].find("ADJA.") != -1:
-                outputData.write(term+" ")
-                statusCode = 2
-        if statusCode == 2:
-            if entry[1].find("N.") != -1:
-                outputData.write(term+"\n")
-                statusCode = 0
+        termExtraction(methodChoice)
 
 inputData.close()
